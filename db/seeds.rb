95840570eb2a3af1300b63a20846764148511292
brian <riceb53@gmail.com>
2021-03-14 10:51:25 -0400

    add seed_dump gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Tag.create!([
+  {name: "how to"},
+  {name: "lifestyle advice"}
+])
+BlogPost.create!([
+  {title: "how to install a toilet", text: "make sure the wax ring is not cracked"},
+  {title: "how to fix your montitor", text: "unplug it then plug it back in"},
+  {title: "my opinions on wegmans", text: "it is excellent"}
+])
+BlogPostTag.create!([
+  {blog_post_id: 1, tag_id: 1},
+  {blog_post_id: 2, tag_id: 1},
+  {blog_post_id: 3, tag_id: 2}
+])
